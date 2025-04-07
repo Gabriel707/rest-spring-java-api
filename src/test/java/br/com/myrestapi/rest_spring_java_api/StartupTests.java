63e6e1bc7b40b76013fc386ed6ef465687055ebf
Gabriel707 <gabrielsant95@gmail.com>
2025-04-07 16:32:10 -0300

    Starting project from scratch

---
+++ src/test/java/br/com/myrestapi/rest_spring_java_api/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.myrestapi.rest_spring_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
